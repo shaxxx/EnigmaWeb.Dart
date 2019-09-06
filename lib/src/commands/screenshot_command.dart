65ff441fbc61eb8bffb7da5b283eb3900a0b6de1
Shax <shaxxx@users.noreply.github.com>
2019-09-06 09:50:39 +0200

    refactor due to enum change

--- lib/src/commands/screenshot_command.dart
+++ lib/src/commands/screenshot_command.dart
@@ -26,38 +26,26 @@ class ScreenshotCommand
   Future<IScreenshotResponse> executeAsync({CancelToken token}) async {
     try {
       String url;
-      switch (type) {
-        case ScreenshotType.all:
-          {
-            url = profile.enigma == EnigmaType.enigma1
-                ? "body?mode=controlScreenShot&blendtype=2"
-                : "grab?format=jpg&mode=all&filename=/tmp/" +
-                    _unixTimeStamp() +
-                    ".jpg";
-            break;
-          }
-        case ScreenshotType.picture:
-          {
-            url = profile.enigma == EnigmaType.enigma1
-                ? "body?mode=controlScreenShot"
-                : "grab?format=jpg&mode=video&v=&filename=/tmp/" +
-                    _unixTimeStamp() +
-                    ".jpg";
-            break;
-          }
-        case ScreenshotType.osd:
-          {
-            url = profile.enigma == EnigmaType.enigma1
-                ? "body?mode=controlFBShot"
-                : "grab?format=jpg&mode=osd&o=&n=&filename=/tmp/" +
-                    _unixTimeStamp() +
-                    ".jpg";
-            break;
-          }
-        default:
-          {
-            throw Exception("Screenshot type not supported.");
-          }
+      if (type == ScreenshotType.all) {
+        url = profile.enigma == EnigmaType.enigma1
+            ? "body?mode=controlScreenShot&blendtype=2"
+            : "grab?format=jpg&mode=all&filename=/tmp/" +
+                _unixTimeStamp() +
+                ".jpg";
+      } else if (type == ScreenshotType.picture) {
+        url = profile.enigma == EnigmaType.enigma1
+            ? "body?mode=controlScreenShot"
+            : "grab?format=jpg&mode=video&v=&filename=/tmp/" +
+                _unixTimeStamp() +
+                ".jpg";
+      } else if (type == ScreenshotType.osd) {
+        url = profile.enigma == EnigmaType.enigma1
+            ? "body?mode=controlFBShot"
+            : "grab?format=jpg&mode=osd&o=&n=&filename=/tmp/" +
+                _unixTimeStamp() +
+                ".jpg";
+      } else {
+        throw Exception("Screenshot type not supported.");
       }
 
       if (profile.enigma == EnigmaType.enigma2) {
@@ -79,16 +67,10 @@ class ScreenshotCommand
         profile,
       );
 
-      switch (type) {
-        case ScreenshotType.osd:
-          {
-            url = "root/tmp/osdshot.png";
-            break;
-          }
-        default:
-          {
-            url = "root/tmp/screenshot.bmp";
-          }
+      if (type == ScreenshotType.osd) {
+        url = "root/tmp/osdshot.png";
+      } else {
+        url = "root/tmp/screenshot.bmp";
       }
 
       var response = await requester.getResponseAsync(
